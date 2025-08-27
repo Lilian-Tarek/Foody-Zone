b4a4ad41d069e812180f1ec4674c47d53184a5e8
Lilian <liliantarek4@gmail.com>
2025-08-27 17:24:43 +0300

    move db.json to api and fix import

--- api/food.js
+++ api/food.js
@@ -1,5 +1,11 @@
-import data from "./db.json";
+import data from "./db.json" assert { type: "json" };
 
 export default function handler(req, res) {
-  res.status(200).json(data.food); 
+  try {
+    res.status(200).json(data.food);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to load data", details: error.message });
+  }
 }
